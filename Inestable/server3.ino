f8dd728039b82361930bcb13ff430e0ba7e6212b
csanz91 <cesarsanz.91@gmail.com>
2015-01-29 12:53:42 +0100

    Add I2C communications

--- Inestable/server3.ino
+++ Inestable/server3.ino
@@ -37,10 +37,22 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
+#include <Wire.h>
+
 // size of buffer used to capture HTTP requests
 #define REQ_BUF_SZ   60
 
-#define NUM_DATA 5
+//Direcciones de los dispositivos conectados al bus I2C
+#define ADDR_I2C_SERVER 1
+#define ADDR_LCD    2
+#define ADDR_SERVER 3
+#define NUM_DATA    5                       //Num de datos que almacenara el array
+
+//Definimos la posicion de cada dato en el array
+#define data1   0
+#define data2   1
+
+float data[NUM_DATA];                           //Array de datos
 
 // MAC address from Ethernet shield sticker under board
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -59,6 +71,8 @@ char StrContains(char *str, char *sfind);
 
 void setup()
 {
+
+    Wire.begin(ADDR_SERVER);                   //Nos unimos al bus I2C con la direccion dada
     // disable Ethernet chip
     pinMode(10, OUTPUT);
     digitalWrite(10, HIGH);
@@ -241,4 +255,30 @@ char StrContains(char *str, char *sfind)
     }
 
     return 0;
+}
+
+//Llama al I2C server para que le devuelva el array de datos
+void askForData(){
+
+    byte data_received[4];
+
+    //Hace una llamada al slave por cada dato que se quiere obtener
+    for(int x=0; x<NUM_DATA; x++){
+        Wire.requestFrom(ADDR_I2C_SERVER, 4);                       //Pide 4 bytes a la direccion del servidor I2C
+
+        //Lee los cuatro bytes de los que se forma el float y los vuelve a unir
+        int i = 0;
+        while(Wire.available())    // slave may send less than requested
+        { 
+            data_received[i] = Wire.read(); // receive a byte as character  
+            i++;
+        }
+        //A union datatypes makes the byte and float elements share the same piece of memory, which enables conversion from a byte array to a float possible
+        union float_tag {byte data_b[4]; float data_fval;} float_Union;    
+        float_Union.data_b[0] = data_received[0];
+        float_Union.data_b[1] = data_received[1];
+        float_Union.data_b[2] = data_received[2];
+        float_Union.data_b[3] = data_received[3];    
+        data[x] = float_Union.data_fval;
+    }
 }
